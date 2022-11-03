a8b4e701e8136e9c03843e7f25c5e124f2a7d05c
김지형 <jihyoung9912@gmail.com>
2022-11-03 20:25:36 +0900

    feat: SingUp 초안

--- src/App.js
+++ src/App.js
@@ -6,13 +6,10 @@ import GlobalStyles from 'styles/GlobalStyles';
 const ResponsiveLayout = lazy(() => import('layouts/responsive.layout'));
 const Searchbar = lazy(() => import('components/Navbar/Searchbar'));
 const SignIn = lazy(() => import('pages/SignIn/SignIn'));
+const SignUp = lazy(() => import('pages/Signup/SignUp'));
 
 const App = () => {
   return (
-    <Router>
-
-const App = () => {
-  return (    
     <Router>
       <GlobalStyles />
       <Suspense fallback={<WaveLoading />}>
@@ -21,6 +18,7 @@ const App = () => {
         </ResponsiveLayout>
         <Routes>
           <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </Suspense>
     </Router>
--- src/components/Navbar/DesktopNavbar.jsx
+++ src/components/Navbar/DesktopNavbar.jsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-import { Box } from '@mui/system';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
@@ -10,9 +8,12 @@ import PeopleAltOutlinedIcon from '@mui/icons-material/PeopleAltOutlined';
 import RecordVoiceOverOutlinedIcon from '@mui/icons-material/RecordVoiceOverOutlined';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import FlexBox from 'components/Common/FlexBox';
+import Link from '@mui/material/Link';
 
 const TabsLib = styled(Tabs)`
-  padding: 0rem 0 2rem 2rem;
+  padding: 3.5rem 0 2rem 2rem;
+  height: 100vh;
+  background: white;
   display: flex;
 `;
 
@@ -21,6 +22,7 @@ const Logo = styled.span`
   font-size: 3rem;
   font-family: 'Pr-Bold';
   font-weight: 600;
+  line-height: 1.5;
 `;
 
 const NavLabel = styled.span`
@@ -51,7 +53,7 @@ const a11yProps = (index) => {
   };
 };
 
-const VerticalTabs = () => {
+const DesktopNavbar = () => {
   const [value, setValue] = React.useState(2);
 
   const handleChange = (event, newValue) => {
@@ -59,163 +61,151 @@ const VerticalTabs = () => {
   };
 
   return (
-    <Box
+    <TabsLib
+      orientation="vertical"
+      variant="scrollable"
+      value={value}
+      onChange={handleChange}
+      aria-label="Vertical tabs example"
       sx={{
-        flexGrow: 2,
-        bgcolor: 'background.paper',
-        display: 'flex',
-        height: '100%',
-        padding: '4.5rem 0 0 0',
+        borderRight: 1,
+        borderColor: 'divider',
+        fontSize: 200,
+        border: 'none',
+        width: 290,
       }}
     >
-      <TabsLib
-        orientation="vertical"
-        variant="scrollable"
-        value={value}
-        onChange={handleChange}
-        aria-label="Vertical tabs example"
+      <Logo>대신</Logo>
+      <NavLabel>MENU</NavLabel>
+      <Tab
+        icon={
+          <HomeOutlinedIcon
+            sx={{
+              fontSize: 30,
+              color: '#282828',
+            }}
+          />
+        }
+        iconPosition="start"
+        label="NOW"
         sx={{
-          borderRight: 1,
-          borderColor: 'divider',
-          fontSize: 200,
-          border: 'none',
-          width: 270,
+          fontSize: 20,
+          fontWeight: 600,
+          color: '#282828',
+          marginTop: 1,
+          justifyContent: 'flex-start',
         }}
-      >
-        <Logo>대신</Logo>
-        <NavLabel>MENU</NavLabel>
-        <Tab
-          icon={
-            <HomeOutlinedIcon
-              sx={{
-                fontSize: 30,
-                color: '#282828',
-              }}
-            />
-          }
-          iconPosition="start"
-          label="NOW"
-          sx={{
-            fontSize: 20,
-            fontWeight: 600,
-            color: '#282828',
-            marginTop: 1,
-            justifyContent: 'flex-start',
-          }}
-        />
-        <Tab
-          icon={
-            <EditOutlinedIcon
-              sx={{
-                fontSize: 30,
-                color: '#282828',
-              }}
-            />
-          }
-          iconPosition="start"
-          label="국민인"
-          sx={{
-            fontSize: 20,
-            fontWeight: 600,
-            color: '#282828',
-            marginTop: 1,
-            justifyContent: 'flex-start',
-          }}
-        />
-        <Tab
-          icon={
-            <PeopleAltOutlinedIcon
-              sx={{
-                fontSize: 30,
-                color: '#282828',
-              }}
-            />
-          }
-          iconPosition="start"
-          label="동아리"
-          sx={{
-            fontSize: 20,
-            fontWeight: 600,
-            color: '#282828',
-            marginTop: 1,
-            justifyContent: 'flex-start',
-          }}
-        />
-        <Tab
-          icon={
-            <RecordVoiceOverOutlinedIcon
-              sx={{
-                fontSize: 30,
-                color: '#282828',
-              }}
-            />
-          }
-          iconPosition="start"
-          label="홍보"
-          sx={{
-            fontSize: 20,
-            fontWeight: 600,
-            color: '#282828',
-            marginTop: 1,
-            justifyContent: 'flex-start',
-          }}
-        />
-        <NavLabel>MY PAGE</NavLabel>
-        <Tab
-          icon={
-            <AccountCircleOutlinedIcon
-              sx={{
-                fontSize: 30,
-                color: '#282828',
-              }}
-            />
-          }
-          iconPosition="start"
-          label="마이페이지"
-          sx={{
-            fontSize: 20,
-            fontWeight: 600,
-            color: '#282828',
-            justifyContent: 'flex-start',
-          }}
-        />
-        <Tab
-          label=" - 스크랩"
-          {...a11yProps(5)}
-          sx={{
-            marginTop: 2,
-            width: 200,
-            fontSize: 20,
-            fontWeight: 600,
-            color: '#282828',
-            justifyContent: 'flex-start',
-          }}
-        />
-        <Tab
-          label=" - 내가 쓴 글"
-          {...a11yProps(6)}
-          sx={{
-            marginTop: 2,
-            width: 231,
-            fontSize: 20,
-            fontWeight: 600,
-            color: '#282828',
-            justifyContent: 'flex-start',
-          }}
-        />
-        <FlexBox margin="4rem 0 0 0.8rem">
-          <LogLogo src={`${process.env.PUBLIC_URL}/images/login.svg`} />
-          <LogLabel>
-            <Link to="/signin">
-              로그인/
-              <br />
-              회원가입
-            </Link>
-          </LogLabel>
-        </FlexBox>
-      </TabsLib>
-    </Box>
+      />
+      <Tab
+        icon={
+          <EditOutlinedIcon
+            sx={{
+              fontSize: 30,
+              color: '#282828',
+            }}
+          />
+        }
+        iconPosition="start"
+        label="국민인"
+        sx={{
+          fontSize: 20,
+          fontWeight: 600,
+          color: '#282828',
+          marginTop: 1,
+          justifyContent: 'flex-start',
+        }}
+      />
+      <Tab
+        icon={
+          <PeopleAltOutlinedIcon
+            sx={{
+              fontSize: 30,
+              color: '#282828',
+            }}
+          />
+        }
+        iconPosition="start"
+        label="동아리"
+        sx={{
+          fontSize: 20,
+          fontWeight: 600,
+          color: '#282828',
+          marginTop: 1,
+          justifyContent: 'flex-start',
+        }}
+      />
+      <Tab
+        icon={
+          <RecordVoiceOverOutlinedIcon
+            sx={{
+              fontSize: 30,
+              color: '#282828',
+            }}
+          />
+        }
+        iconPosition="start"
+        label="홍보"
+        sx={{
+          fontSize: 20,
+          fontWeight: 600,
+          color: '#282828',
+          marginTop: 1,
+          justifyContent: 'flex-start',
+        }}
+      />
+      <NavLabel>MY PAGE</NavLabel>
+      <Tab
+        icon={
+          <AccountCircleOutlinedIcon
+            sx={{
+              fontSize: 30,
+              color: '#282828',
+            }}
+          />
+        }
+        iconPosition="start"
+        label="마이페이지"
+        sx={{
+          fontSize: 20,
+          fontWeight: 600,
+          color: '#282828',
+          justifyContent: 'flex-start',
+        }}
+      />
+      <Tab
+        label=" - 스크랩"
+        {...a11yProps(5)}
+        sx={{
+          marginTop: 2,
+          width: 200,
+          fontSize: 20,
+          fontWeight: 600,
+          color: '#282828',
+          justifyContent: 'flex-start',
+        }}
+      />
+      <Tab
+        label=" - 내가 쓴 글"
+        {...a11yProps(6)}
+        sx={{
+          marginTop: 2,
+          width: 231,
+          fontSize: 20,
+          fontWeight: 600,
+          color: '#282828',
+          justifyContent: 'flex-start',
+        }}
+      />
+      <FlexBox margin="4rem 0 0 0.8rem">
+        <LogLogo src={`${process.env.PUBLIC_URL}/images/login.svg`} />
+        <LogLabel>
+          로그인/
+          <br />
+          회원가입
+        </LogLabel>
+      </FlexBox>
+    </TabsLib>
   );
 };
 
-export default VerticalTabs;
+export default DesktopNavbar;
