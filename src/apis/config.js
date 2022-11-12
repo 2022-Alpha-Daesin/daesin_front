87209c6ad30ccbc0dfa9fd10a2df665a9ccb5d65
김지형 <jihyoung9912@gmail.com>
2022-11-13 02:49:55 +0900

    feat: SignupMutation

--- src/apis/config.js
+++ src/apis/config.js
@@ -3,9 +3,9 @@ import { baseUrl } from '../constants/URLS';
 import { useEffect } from 'react';
 
 const createInstance = () => {
-  const timeout = 1000 * 30;
+  const timeout = 1000 * 10;
   const headers = {};
-  // 30초동안 응답이 없으면 에러 --> 인터넷 느려서 마지막 설정
+  // 10초동안 응답이 없으면 에러 --> 인터넷 느려서 마지막 설정
 
   return axios.create({
     baseURL: baseUrl,
--- src/queries/auth/useSignUpMutation.js
+++ src/queries/auth/useSignUpMutation.js
@@ -1,23 +1,10 @@
 import { useMutation } from '@tanstack/react-query';
-// import toast from 'react-hot-toast';
 import userRelatedAPI from 'apis/userRelatedAPI';
-import { useNavigate } from 'react-router-dom';
-// import errorMsgHandler from 'utils/errorMsgHandler';
-// import messages from 'constants/messages';
 
 const useSignUpMutation = () => {
-  const navigate = useNavigate();
-
-  return useMutation(
-    (payload) => {
-      return userRelatedAPI.postSignup(payload);
-    },
-    {
-      onSuccess: () => {
-        navigate('/signup');
-      },
-    },
-  );
+  return useMutation((payload) => {
+    return userRelatedAPI.postSignup(payload);
+  });
 };
 
 export default useSignUpMutation;
