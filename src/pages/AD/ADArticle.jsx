03b1c161f2ac6d8b551c2bb65a6b10377dce8b48
soyekwon <soye0710@naver.com>
2022-11-28 02:11:17 +0900

    fix: 홍보 글 내용 적을 시 width 줄어드는 issue

--- src/pages/AD/ADArticle.jsx
+++ src/pages/AD/ADArticle.jsx
@@ -1,9 +1,11 @@
 import { useParams } from "react-router-dom";
 import { FlexBox, FlexTextBox } from "components/Common";
+import useDate from "hooks/useDate";
 import Searchbar from "components/Navbar/Searchbar";
 import styled from "styled-components";
 import { Icon } from "semantic-ui-react";
 import useADQuery from "queries/AD/useADQuery";
+import { useUserInfoQuery } from "queries/auth";
 
 const Img = styled.img`
   width: 80%;
@@ -18,20 +20,24 @@ const Line = styled.div`
 
 const ADArticle = () => {
   const { id } = useParams();
-  const { data } = useADQuery(id);
-  console.log(data);
+  const { data: ad } = useADQuery(id);
+  const { data: user } = useUserInfoQuery();
+  const date = useDate(ad.postupdated_at);
+
   return (
     <FlexBox width="100%" column>
-      <Searchbar />
-      <FlexBox width="100%" margin="2.5rem 0 0 0" gap="5%">
+      <FlexBox margin="1.9rem 0 0 0">
+        <Searchbar />
+      </FlexBox>
+      <FlexBox width="98%" margin="2.5rem 0 0 0" gap="5%">
         <Line />
-        <FlexBox column gap="2rem" margin="2rem 0 0 0">
+        <FlexBox width="98%" column gap="2rem" margin="2rem 0 0 0">
           <FlexBox width="100%" position="relative">
             <FlexBox width="3rem" height="3rem" borderRadius="50%" background="#FFC8C8" />
             <FlexBox column gap="0.2rem" margin="0.2rem 0 0 0.8rem">
-              <FlexTextBox fontSize="1.25rem">{data.post.title}</FlexTextBox>
+              <FlexTextBox fontSize="1.25rem">{user.nickname}</FlexTextBox>
               <FlexTextBox fontSize="0.85rem" color="#717171">
-                2022.10.14
+                {date}
               </FlexTextBox>
             </FlexBox>
             <FlexBox margin="1rem 0 0 0" position="absolute" right="0" width="5rem">
@@ -39,13 +45,13 @@ const ADArticle = () => {
               <Icon disabled name="paperclip" size="large" link />
             </FlexBox>
           </FlexBox>
-          <FlexTextBox fontSize="2rem">{data.post.title}</FlexTextBox>
+          <FlexTextBox fontSize="2rem">{ad.post.title}</FlexTextBox>
           <FlexBox width="100%" center column gap="2rem">
             <FlexBox width="86%" column center background="#DCDCDC">
               <Img src={`${process.env.PUBLIC_URL}/images/ADArticle.png`} />
             </FlexBox>
             <FlexTextBox width="100%" fontSize="1.1rem">
-              {data.post.content}
+              {ad.post.content}
             </FlexTextBox>
           </FlexBox>
           <FlexTextBox color="#679AFF">#동방있음</FlexTextBox>
