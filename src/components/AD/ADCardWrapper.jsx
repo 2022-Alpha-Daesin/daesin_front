206d45f70fb0556e33446da8b092f62f04dcbe95
soyekwon <soye0710@naver.com>
2022-12-10 02:53:15 +0900

    feat: 홍보 종류 카테고리 연결

--- src/components/AD/ADCardWrapper.jsx
+++ src/components/AD/ADCardWrapper.jsx
@@ -20,16 +20,109 @@ const Btn = styled.button`
 
 const ADCardWrapper = () => {
   const [isClicked, setClick] = useState([true, false, false, false, false]);
-  const [id, setId] = useState(0);
+  const [category, setCategory] = useState(0);
   const { data: ADList } = useADListQuery();
 
+  console.log(ADList);
+
+  const handleClick = (id, idd) => {
+    setClick(
+      isClicked.map((data, idx) => {
+        if (data === true) {
+          data = false;
+        }
+        if (idx === id) {
+          data = !data;
+        }
+
+        return data;
+      }),
+    );
+    console.log(isClicked);
+    setCategory(idd);
+  };
+
+  const insertCard = () => {
+    const newArr = [];
+    if (category === 0) {
+      for (let i = 0; i < ADList.results.length; i += 1) {
+        newArr.push(
+          <ADCard
+            key={ADList.results[i].id}
+            title={ADList.results[i].title}
+            content={ADList.results[i].content}
+            id={ADList.results[i].id}
+          />,
+        );
+      }
+    } else {
+      for (let i = 0; i < ADList.results.length; i += 1) {
+        if (ADList.results[i].tags.length === 0) {
+          continue;
+        }
+        if (ADList.results[i].tags[0].tag === category)
+          newArr.push(
+            <ADCard
+              key={ADList.results[i].id}
+              title={ADList.results[i].title}
+              content={ADList.results[i].content}
+              id={ADList.results[i].id}
+            />,
+          );
+      }
+    }
+
+    return newArr;
+  };
+
   return (
-    <FlexBox gap="1.5rem" wrap="wrap">
-      {"results" in ADList &&
-        ADList.results.map((data, idx) => (
-          <ADCard key={idx} title={data.title} content={data.content} id={data.id} />
-        ))}
-    </FlexBox>
+    <>
+      <FlexBox gap="1rem">
+        <Btn
+          isClicked={isClicked[0]}
+          onClick={() => {
+            handleClick(0, 0);
+          }}
+        >
+          ALL
+        </Btn>
+        <Btn
+          isClicked={isClicked[1]}
+          onClick={() => {
+            handleClick(1, 2);
+          }}
+        >
+          동아리
+        </Btn>
+        <Btn
+          isClicked={isClicked[2]}
+          onClick={() => {
+            handleClick(2, 12);
+          }}
+        >
+          알파프로젝트
+        </Btn>
+        <Btn
+          isClicked={isClicked[3]}
+          onClick={() => {
+            handleClick(3, 13);
+          }}
+        >
+          알바
+        </Btn>
+        <Btn
+          isClicked={isClicked[4]}
+          onClick={() => {
+            handleClick(4, 0);
+          }}
+        >
+          기타
+        </Btn>
+      </FlexBox>
+      <FlexBox gap="1.5rem" wrap="wrap">
+        {insertCard()}
+      </FlexBox>
+    </>
   );
 };
 export default ADCardWrapper;
